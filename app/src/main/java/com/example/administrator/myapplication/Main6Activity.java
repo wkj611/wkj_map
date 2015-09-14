6aca9fcc2026b337e0aa23d41f5cf5f152238db2
wkj <996825244@qq.com>
2015-09-14 20:50:54 +0800

    add sdcard read and write

--- app/src/main/java/com/example/administrator/myapplication/Main6Activity.java
+++ app/src/main/java/com/example/administrator/myapplication/Main6Activity.java
@@ -3,24 +3,30 @@ package com.example.administrator.myapplication;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main6Activity extends AppCompatActivity implements View.OnClickListener{
     Button read,write;
     EditText edread,edwrite;
-    String FILE_NAME = "/wkj.txt";
+    String FILE_NAME = "wkj.txt";
 
 
     @Override
@@ -73,11 +79,13 @@ public class Main6Activity extends AppCompatActivity implements View.OnClickList
     private void mywrite(String s) {
         try{
             if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-                File sdCardDir = Environment.getExternalStorageDirectory();
-                File targetFile = new File(sdCardDir.getCanonicalPath() + FILE_NAME);
-                System.out.println(sdCardDir.getCanonicalPath());
-                RandomAccessFile randomAccessFile = new RandomAccessFile(targetFile,"rw");
-                randomAccessFile.seek(targetFile.length());
+                File sdCardDir = new File(getSDCardPath());
+                File targetFile = new File(sdCardDir, FILE_NAME);
+                if(!targetFile.exists()){
+                    targetFile.createNewFile();
+                    targetFile.setWritable(true);
+                }
+                FileOutputStream randomAccessFile = new FileOutputStream(targetFile);
                 randomAccessFile.write(s.getBytes());
                 randomAccessFile.close();
             }
@@ -91,8 +99,9 @@ public class Main6Activity extends AppCompatActivity implements View.OnClickList
         int readnum;
         try{
             if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-                File sdCardDir = Environment.getExternalStorageDirectory();
-                FileInputStream fileInputStream = new FileInputStream(sdCardDir.getCanonicalPath()+FILE_NAME);
+                File sdCardDir = new File(getNormalSDCardPath());
+                File targetFile = new File(sdCardDir, FILE_NAME);
+                FileInputStream fileInputStream = new FileInputStream(targetFile);
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
                 StringBuilder stringBuilder = new StringBuilder("");
                 String line = null;
@@ -107,6 +116,62 @@ public class Main6Activity extends AppCompatActivity implements View.OnClickList
         }
         return null;
 
+    }
+    public static String getSDCardPath() {
+        String cmd = "cat /proc/mounts";
+        Runtime run = Runtime.getRuntime();// 返回与当前 Java 应用程序相关的运行时对象
+        BufferedInputStream in=null;
+        BufferedReader inBr=null;
+        try {
+            Process p = run.exec(cmd);// 启动另一个进程来执行命令
+            in = new BufferedInputStream(p.getInputStream());
+            inBr = new BufferedReader(new InputStreamReader(in));
+
+
+            String lineStr;
+            while ((lineStr = inBr.readLine()) != null) {
+                // 获得命令执行后在控制台的输出信息
+                Log.v("getSDCardPath", lineStr);
+                if (lineStr.contains("sdcard")
+                        && lineStr.contains(".android_secure")) {
+                    String[] strArray = lineStr.split(" ");
+                    if (strArray != null && strArray.length >= 5) {
+                        String result = strArray[1].replace("/.android_secure",
+                                "");
+                        return result;
+                    }
+                }
+                // 检查命令是否执行失败。
+                if (p.waitFor() != 0 && p.exitValue() == 1) {
+                    // p.exitValue()==0表示正常结束，1：非正常结束
+                    Log.e("getSDCardPath", "命令执行失败!");
+                }
+            }
+        } catch (Exception e) {
+            Log.e("getSDCardPath", e.toString());
+            //return Environment.getExternalStorageDirectory().getPath();
+        }finally{
+            try {
+                if(in!=null){
+                    in.close();
+                }
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+                if(inBr!=null){
+                    inBr.close();
+                }
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return Environment.getExternalStorageDirectory().getPath();
+    }
+    public static String getNormalSDCardPath(){
+        return Environment.getExternalStorageDirectory().getPath();
     }
 
 }
